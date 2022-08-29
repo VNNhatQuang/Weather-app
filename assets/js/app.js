4b2720942f1c20bf01d0f07de7560dcc8687deaa
Vo Ngoc Nhat Quang <quangvip2611@gmail.com>
2022-08-29 11:18:43 +0700

    Update get location

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,40 @@
-import { Weather, getIcon, getWindSpeed, getHumidity, getCountry, getTemp, getTempMinMax, getDescription } from './modules/handle.js';
+import { Weather, getIcon, getWindSpeed, getHumidity, getCityCountry, getTemp, getTempMinMax, getDescription, weatherLocation } from './modules/handle.js';
+
 
 var appId = '97234812a700ced559577a99acae33dc';
 var search = document.querySelector('.search-icon');
 var areaSearch = document.querySelector('html');
+var lat;
+var lon;
 
 
+// GET LOCATION
+var getLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert('Geolocation is not supported by this browser.');
+    }
+}
+
+var showPosition = position => {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude
+    console.log(lat);
+    console.log(lon);
+    weatherLocation(lat, lon);
+}
+
+getLocation();
+// showPosition();
+// console.log(lat);
+// console.log(lon);
+// weatherLocation(lat, lon)
+
+
+
+
+// SEARCH LOCATION
 search.onclick = function() {
     Weather();
 }
--- assets/js/modules/handle.js
+++ assets/js/modules/handle.js
@@ -1,6 +1,7 @@
 var appId = '97234812a700ced559577a99acae33dc';
 var search = document.querySelector('.search-icon');
 
+
 function Weather() {
     var city = document.querySelector('input[type="text"]');
     var url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${appId}`;
@@ -21,11 +22,10 @@ function Weather() {
             };
             console.log(htmls);
             getIcon(htmls.icon);
-            // getCityCountry(htmls.city, htmls.country);
             getTemp(htmls.temp);
             getWindSpeed(htmls.windSpeed);
             getHumidity(htmls.humidity);
-            getCountry(htmls.country);
+            getCityCountry(htmls.city, htmls.country);
             getTempMinMax(htmls.tempMin, htmls.tempMax);
             getDescription(htmls.description);
         })
@@ -50,9 +50,9 @@ var getHumidity = humidity => {
     humidityDisplay.innerHTML = `${humidity}%`;
 }
 
-var getCountry = country => {
-    var countryDisplay = document.querySelector('.country>span');
-    countryDisplay.innerHTML = country;
+var getCityCountry = (city, country) => {
+    var cityCountryDisplay = document.querySelector('.city-country>span');
+    cityCountryDisplay.innerHTML = ` ${city}, ${country}`;
 }
 
 var getTemp = temp => {
@@ -74,5 +74,39 @@ var getDescription = description => {
 
 
 
+// GET LOCATION
 
-export { Weather, getIcon, getWindSpeed, getHumidity, getCountry, getTemp, getTempMinMax, getDescription };
+function weatherLocation(lat, lon) {
+    var city = document.querySelector('input[type="text"]');
+    var url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            var htmls = {
+                city: `${data.name}`,
+                country: `${data.sys.country}`,
+                description: `${data.weather[0].description}`,
+                icon: `${data.weather[0].icon}`,
+                temp: `${(data.main.temp-273.15).toFixed()}`,
+                tempMin: `${(data.main.temp_min-273.15).toFixed()}`,
+                tempMax: `${(data.main.temp_max-273.15).toFixed()}`,
+                windSpeed: `${data.wind.speed}`,
+                humidity: `${data.main.humidity}`,
+            };
+            console.log(htmls);
+            getIcon(htmls.icon);
+            getTemp(htmls.temp);
+            getWindSpeed(htmls.windSpeed);
+            getHumidity(htmls.humidity);
+            getCityCountry(htmls.city, htmls.country);
+            getTempMinMax(htmls.tempMin, htmls.tempMax);
+            getDescription(htmls.description);
+        })
+        .catch(err => alert("Wrong city name!"));
+}
+
+
+
+
+export { Weather, getIcon, getWindSpeed, getHumidity, getCityCountry, getTemp, getTempMinMax, getDescription, weatherLocation };
